c557a2e891827dfd8bc2949e817589de27c1e549
Daniellumbu <128616598+Daniellumbu@users.noreply.github.com>
2025-08-03 15:02:07 -0500

    Update flask_app.py
    update

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -1,10 +1,8 @@
-app=Flask(__name__)
-@app.route('/')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
-
-
-if __name__=="__main__":
-    app.run(host='0.0.0.0',port=2211)
-from flask_app import app as application  # noqa
